b6f7bed0aeb85cd33a4e4b0f7baa5c197b9ddf6d
bernadustoro <bernadustoro@gmail.com>
2026-04-20 17:37:45 +0700

    fix

--- app/core/config.py
+++ app/core/config.py
@@ -3,8 +3,9 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     DATABASE_URL: str
     GEMINI_API_KEY: str
-    WHATSAPP_VERIFY_TOKEN: str
-    WHATSAPP_TOKEN: str
+    TWILIO_ACCOUNT_SID: str
+    TWILIO_AUTH_TOKEN: str
+    TWILIO_WHATSAPP_NUMBER: str
     model_config = SettingsConfigDict(env_file=".env")
 
 settings = Settings()
